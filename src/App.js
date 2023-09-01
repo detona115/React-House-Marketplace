6320f4fbb0edd42a94137c2c857888ab6a6c42a1
ANDY <andykiaka@gmail.com>
2023-09-01 00:58:59 -0400

    added PrivateRoute Spinener useAuthStatus components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
+import PrivateRoute from './components/PrivateRoute';
 import Explore from './pages/Explore';
 import Offer from './pages/Offer';
 import Profile from './pages/Profile';
@@ -13,12 +14,14 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Explore />} />
-        <Route path="/offer" element={<Offer />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path='/' element={<Explore />} />
+        <Route path='/offer' element={<Offer />} />
+        <Route path='/profile' element={<PrivateRoute />}>
+          <Route path='/profile' element={<Profile />} />
+        </Route>
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
       </Routes>
       <Navbar />
 
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,105 @@
 import { useEffect, useState } from 'react';
-import { getAuth } from 'firebase/auth';
+import { getAuth, updateProfile } from 'firebase/auth';
+import { updateDoc, doc } from 'firebase/firestore';
+import { db } from '../firebase.config';
+import { useNavigate, Link } from 'react-router-dom';
+import { toast } from 'react-toastify';
 
 function Profile() {
-  const [user, setUser] = useState(null);
-
+  // instantiating a firebase authentication object
   const auth = getAuth();
+  const [formData, setFormData] = useState({
+    name: auth.currentUser.displayName,
+    email: auth.currentUser.email,
+  });
+  const [changeDetails, setChangeDetails] = useState(false);
 
-  useEffect(() => {
-    setUser(auth.currentUser);
-    console.log(auth.currentUser);
-    // eslint-disable-next-line
-  }, []);
+  const { name, email } = formData;
 
-  return user ? <h1>{user.displayName}</h1> : 'Not logged';
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    auth.signOut();
+    navigate('/');
+  }; // logging out from firebase
+
+  const onSubmit = async () => {
+    try {
+      if (auth.currentUser.displayName !== name) {
+        // Update the display name
+        await updateProfile(auth.currentUser, {
+          displayName: name,
+        });
+
+        // Update in firestore
+        const userRef = doc(db, 'users', auth.currentUser.uid);
+        await updateDoc(userRef, { name: name });
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Couldn't update profile details");
+    }
+  };
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }));
+  };
+
+  // useEffect(() => {
+  //   setFormData(auth.currentUser);
+  //   console.log(auth.currentUser);
+  //   // eslint-disable-next-line
+  // }, []);
+
+  return (
+    <div className='profile'>
+      <header className='profileHeader'>
+        <p className='pageHeader'>My Profile</p>
+        <button type='button' className='logOut' onClick={onLogout}>
+          Logout
+        </button>
+      </header>
+
+      <main>
+        <div className='profileDetailsHeader'>
+          <p className='profileDetailsText'>Personal Details</p>
+          <p
+            className='changePersonalDetails'
+            onClick={() => {
+              changeDetails && onSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? 'done' : 'change'}
+          </p>
+        </div>
+
+        <div className='profileCard'>
+          <form>
+            <input
+              type='text'
+              id='name'
+              className={!changeDetails ? 'profileName' : 'profileNameActive'}
+              disabled={!changeDetails}
+              value={name}
+              onChange={onChange}
+            />
+            <input
+              type='text'
+              id='email'
+              className={!changeDetails ? 'profileEmail' : 'profileEmailActive'}
+              disabled={!changeDetails}
+              value={email}
+              onChange={onChange}
+            />
+          </form>
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default Profile;
